14af5e6d46efa142eabd66c1117011a6c5e414bf
Alex Valentiner <alex.valentiner@hotmail.com>
2022-03-01 19:12:55 +0100

    Added the code from the picture

---
+++ lab01/exercises/reflex_agent_with_state.py
@@ -0,0 +1,74 @@
+A = 'A'
+B = 'B'
+state = {}
+action = None
+model = {A: None, B: None}
+
+RULE_ACTION = {
+    1: 'Suck',
+    2: 'Right',
+    3: 'Left',
+    4: 'NoOp'
+}
+
+rules = {
+    (A, 'Dirty'): 1,
+    (B, 'Dirty'): 1,
+    (A, 'Clean'): 2,
+    (B, 'Clean'): 3,
+    (A, B, 'Clean'): 4,
+}
+
+environment = {
+    A: 'Dirty',
+    B: 'Dirty',
+    'Current': A
+}
+
+def interpret_input(input):
+    return input
+
+def rule_match(state, rules):
+    rule = rules.get(tuple(state))
+    return rule
+
+def update_state(state, action, percept):
+    (location, status) = percept
+    state = percept
+    if model[A] == model[B] == 'Clean':
+        state = (A, B, 'Clean')
+    model[location] = status
+    return state
+
+def reflex_agent_with_state(percept):
+    global state, action
+    state = update_state(state, action, percept)
+    rule = rule_match(state, rules)
+    action = RULE_ACTION[rule]
+    return action
+
+def sensors():
+    location = environment['Current']
+    return (location, environment[location])
+
+def actuators(action):
+    location = environment['Current']
+    if action == 'Suck':
+        environment[location] = 'Clean'
+    elif action == 'Right' and location == A:
+        environment['Current'] = B
+    elif action == 'Left' and location == B:
+        environment['Current'] = A
+
+def run(n):
+    print('     Current\t\t\t   New')
+    print('location status\t\taction\t location    status')
+    for i in range(1, n):
+        (location, status) = sensors()
+        print("{:10s}{:15s}".format(location, status), end='')
+        action = reflex_agent_with_state(sensors())
+        actuators(action)
+        (location, status) = sensors()
+        print("{:11s}{:9s}{:8s}".format(action, location, status))
+
+run(10)
